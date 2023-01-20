a7ee97bb049edd97db647932c5bd95730103f794
ALTCODE255 <68763594+ALTCODE255@users.noreply.github.com>
2023-01-19 21:33:47 -0800

    Added done message

--- filename-metadata_converter.py
+++ filename-metadata_converter.py
@@ -3,11 +3,14 @@ import piexif
 from datetime import datetime
 folder_path = input("Absolute path of image folder: ")
 
-acceptable_file_extensions = [".jpg", ".png", ".jpeg", ".tif", ".tiff"]
+acceptable_file_extensions = [".jpg", ".jpeg", ".tif", ".tiff"]
+unsupported_files = 0
 
 for image in os.listdir(folder_path):
 	img_extension = os.path.splitext(image)[1]
-	if img_extension not in acceptable_file_extensions: continue
+	if img_extension not in acceptable_file_extensions:
+		unsupported_files += 1
+		continue
 	img_path = f"{folder_path}/{image}"
 	datetime_object = datetime.strptime(image[:-4], '%Y%m%d_%H%M%S')
 	md_datetime = datetime_object.strftime("%Y:%m:%d %H:%M:%S")
@@ -17,3 +20,5 @@ for image in os.listdir(folder_path):
 	exif_dict['Exif'][piexif.ExifIFD.DateTimeDigitized] = md_datetime
 	exif_bytes = piexif.dump(exif_dict)
 	piexif.insert(exif_bytes, img_path)
+
+input(f"Done! {unsupported_files} file(s) remain unchanged (file extension not supported). Press enter to exit.")
